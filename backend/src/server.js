8773dcc6a50569f41b1c2b7dc079015fa51b2638
Hemnath M <hemnath04112004@gmail.com>
2025-07-19 13:56:48 +0530

    prepared for the deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,37 @@ import notesRoutes from "./routes/notesRouter.js";
 import { connectDB } from "./config/db.js"; 
 import rateLimitter from "./middleware/rateLimitter.js";
 import cors from 'cors';
+import path from 'path';
 
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(cors(
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
   {
     origin: "http://localhost:5173",
   }
 )); //middleware 
+
+};
+
 app.use(express.json()); //middleware 
 app.use(rateLimitter); //middleware
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+};
+
+
 
 
 connectDB().then(() =>{
